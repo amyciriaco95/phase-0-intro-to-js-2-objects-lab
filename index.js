b3fdac097a67597ee353cabf28e3a4b3c5166d8e
amyciriaco95 <amyciriaco95@gmail.com>
2023-02-07 19:33:58 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Jane Doe",
+    streetAddress: "46 Riverside Drive",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updateInfo = {...employee};
+    updateInfo[key] = value;
+    return updateInfo;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
